59e6aa59b4334d4228ca6c06580c1c3d5ee7a8c5
Ryan Ramadhan <92355043+RyanRamadhan11@users.noreply.github.com>
2022-03-04 21:57:19 +0700

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,80 @@
+var nama = "ryan"
+console.log(nama)
+console.log('Hello, world!')
+let var1 = 'nama';
+let var2 = 'name';
+let var3 = 'nami';
+let var4 = 'nami';
+let var5 = 10;
+let var6 = 15;
+let var7 = 15;
+
+const gebetan = {
+  name: "joko",
+  isOnline: true,
+}
+
+if(gebetan.isOnline){
+  console.log('Hello, ${gebetan.name}'); 
+  console.log("lagi ngapain nih");
+};
+
+ console.log(var6 + var5);
+ console.log(var7 % var5);
+ const jumlah = var7++;
+ console.log(jumlah);
+ console.log(var5);
+ var1 += ' saya adalah ryan';
+ console.log(var1);
+
+if(var1 != var2 && var1 != var2){
+  console.log(true);
+}else{
+  console.log(false);
+}
+
+if(var1 || var2){
+  console.log(true);
+}else{
+  console.log(false);
+}
+
+//new
+console.log('ok' && 3>1 && 3<2 && 'ya');
+
+const x = 10; 
+console.log(x === 11 ? "Sebelas" : "Sepuluh");
+
+ console.log(var6 <= var5 || true);
+ console.log(1>2 && (10+20));
+
+if(2){
+  console.log(true);
+}else{
+  console.log(false);
+}
+
+
+
+console.log('Hello, world!')
+
+//variabel var
+var name = "ryan";
+if (true){
+  var name = "wawan";
+}
+var name = "jaka makan";
+console.log(name);
+
+//variabel var
+let umure = 20;
+umure = 90;
+console.log(umure);
+
+//variabel const
+const hobie = 'makan';
+if (true){
+  const hobie = "wawan";
+  console.log(hobie);
+}
+console.log(hobie);
