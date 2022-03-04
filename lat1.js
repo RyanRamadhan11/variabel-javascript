65c2a722f5f9cb59edebb9589641434b2da69e4d
Ryan Ramadhan <92355043+RyanRamadhan11@users.noreply.github.com>
2022-03-04 21:59:36 +0700

    Add files via upload

---
+++ lat1.js
@@ -0,0 +1,102 @@
+var nama = "ryan"
+console.log(nama)
+console.log('Hello, world!')
+let var1 = 'nama';
+let var2 = 'name';
+let var3 = 'nami';
+let var4 = 'nami';
+let var5 = 10;
+let var6 = 15;
+let var7 = 15;
+
+const gebetan = {
+  name: "joko",
+  isOnline: true,
+}
+
+if(gebetan.isOnline){
+  console.log('Hello, ${gebetan.name}'); 
+  console.log("lagi ngapain nih");
+};
+
+ console.log(var6 + var5);
+ console.log(var7 % var5);
+ const jumlah = var7++;
+ console.log(jumlah);
+ console.log(var5);
+ var1 += ' saya adalah ryan';
+ console.log(var1);
+
+if(var1 != var2 && var1 != var2){
+  console.log(true);
+}else{
+  console.log(false);
+}
+
+if(var1 || var2){
+  console.log(true);
+}else{
+  console.log(false);
+}
+
+//new
+console.log('ok' && 3>1 && 3<2 && 'ya');
+
+const x = 10; 
+console.log(x === 11 ? "Sebelas" : "Sepuluh");
+
+ console.log(var6 <= var5 || true);
+ console.log(1>2 && (10+20));
+
+if(2){
+  console.log(true);
+}else{
+  console.log(false);
+}
+
+
+
+console.log('Hello, world!')
+
+//variabel var
+var name = "ryan";
+if (true){
+  var name = "wawan";
+}
+var name = "jaka makan";
+console.log(name);
+
+//variabel var
+let umure = 20;
+umure = 90;
+console.log(umure);
+
+//variabel const
+const hobie = 'makan';
+if (true){
+  const hobie = "wawan";
+  console.log(hobie);
+}
+console.log(hobie);
+
+//array 
+let alat_musik = ['biola','gitar','drum'];
+
+alat_musik[0]= 'piano';
+alat_musik.push('makan');
+alat_musik.pop();
+
+
+console.log(alat_musik);
+
+//objek
+const siswa ={
+  nama : "ryan",
+  umur : 21,
+  juruan : "IT",
+}
+
+siswa.umur = 20;
+siswa.hobi = "makan";
+
+console.log(siswa);
